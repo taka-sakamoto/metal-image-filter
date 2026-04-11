65edcd68ed0d47fb5d731d01c21ded806f425177
taka-sakamoto <goodspeed855@gmail.com>
2026-04-11 11:22:54 +0900

    Initial Commit

---
+++ ImageFilterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageFilterApp
+//
+//  Created by Takayuki Sakamoto on 2026/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageFilterApp/ImageFilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFilterAppApp.swift
+//  ImageFilterApp
+//
+//  Created by Takayuki Sakamoto on 2026/04/11.
+//
+
+import SwiftUI
+
+@main
+struct ImageFilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
